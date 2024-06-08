28535c6f835a599f795e3f5f8ebb1fccee983bbf
giannims <giannims2002@gmail.com>
2024-06-08 21:49:15 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Map from "./components/Map";
+import Account from "./components/Account";
+import Settings from "./components/Settings";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarActiveTintColor: '#e91e63',
+                }}>
+                <Tab.Screen
+                    name="Map"
+                    component={Map}
+                    options={{
+                        tabBarLabel: 'Map',
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="map" color={color} size={size}/>
+                        ),
+                    }}/>
+                <Tab.Screen
+                    name="Account"
+                    component={Account}
+                    options={{
+                        tabBarLabel: 'Account',
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="account" color={color} size={size}/>
+                        ),
+                    }}/>
+                <Tab.Screen
+                    name="Settings"
+                    component={Settings}
+                    options={{
+                        tabBarLabel: 'Settings',
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="cog" color={color} size={size}/>
+                        ),
+                    }}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
